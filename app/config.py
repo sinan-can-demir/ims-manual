50596151bc6a572a36674fea7acd1799a6d27e72
eisensenpou <fanflowphoto@gmail.com>
2026-03-29 13:21:10 -0500

    chore: add warehouse path to app.config

--- app/config.py
+++ app/config.py
@@ -5,3 +5,4 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 DATA_LAKE_ROOT = Path(os.getenv("DATA_LAKE_ROOT", BASE_DIR / "data_lake"))
 INVENTORY_EVENTS_ROOT = DATA_LAKE_ROOT / "inventory_events"
 CHECKPOINT_FILE = DATA_LAKE_ROOT / "checkpoints.json"
+WAREHOUSE_ROOT = Path(os.getenv("WAREHOUSE_ROOT", BASE_DIR / "warehouse"))
