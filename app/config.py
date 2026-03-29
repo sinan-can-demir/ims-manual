6cd5107e6d4242ed20711520e9c87e77b8357516
eisensenpou <fanflowphoto@gmail.com>
2026-03-29 13:25:48 -0500

    chore: add warehouse date configurations and reformat the code

--- app/config.py
+++ app/config.py
@@ -2,7 +2,17 @@ from pathlib import Path
 import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# -----------------
+# Data Lake
+# -----------------
 DATA_LAKE_ROOT = Path(os.getenv("DATA_LAKE_ROOT", BASE_DIR / "data_lake"))
 INVENTORY_EVENTS_ROOT = DATA_LAKE_ROOT / "inventory_events"
 CHECKPOINT_FILE = DATA_LAKE_ROOT / "checkpoints.json"
+
+# ------------------
+# Warehouse
+# ------------------
 WAREHOUSE_ROOT = Path(os.getenv("WAREHOUSE_ROOT", BASE_DIR / "warehouse"))
+WAREHOUSE_START_DATE = os.getenv("WAREHOUSE_START_DATE", "2020-01-01")
+WAREHOUSE_END_DATE = os.getenv("WAREHOUSE_END_DATE", "2030-12-31")
